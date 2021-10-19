023609f126bd051c8a787d1f54bbf2702811e37a
carlomigueldy <carlomigueldy@gmail.com>
2021-10-19 08:06:55 +0800

    Define vue router and routes

--- web-previewer/src/main.js
+++ web-previewer/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import router from "./router/index";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
