3248685dfa1198c74e1a7cff45b39d1748febaf3
carlomigueldy <carlomigueldy@gmail.com>
2021-10-22 10:30:04 +0800

    Add TypeScript support for Hardhat project

--- pixel-avatar-hardhat/scripts/deploy.ts
+++ pixel-avatar-hardhat/scripts/deploy.ts
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -13,9 +13,9 @@ async function main() {
   console.log("Token address:", token.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
