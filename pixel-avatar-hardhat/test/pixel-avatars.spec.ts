c2956fff1a3ec1c8c76607e18706d1cb87b5e15a
carlomigueldy <carlomigueldy@gmail.com>
2021-10-23 11:20:30 +0800

    Add tests for modifiers

--- pixel-avatar-hardhat/test/pixel-avatars.spec.ts
+++ pixel-avatar-hardhat/test/pixel-avatars.spec.ts
@@ -1,13 +1,50 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { PixelAvatars } from "../typechain";
 
 describe("PixelAvatars", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const PixelAvatars = await ethers.getContractFactory("PixelAvatars");
-    const pixelAvatars = await PixelAvatars.deploy();
-    await pixelAvatars.deployed();
+  let contract: PixelAvatars;
 
-    expect(await pixelAvatars.deployed()).to.be.not.undefined.and.to.be.not
-      .null;
+  beforeEach(async () => {
+    const PixelAvatars = await ethers.getContractFactory("PixelAvatars");
+    contract = await PixelAvatars.deploy();
+  });
+
+  it("Should return the new greeting once it's changed", async function () {
+    await contract.deployed();
+
+    expect(await contract.deployed()).to.be.not.undefined.and.to.be.not.null;
+  });
+
+  describe("mintWithDevDaoToken", () => {
+    describe("modifier: validDevDaoToken", async () => {
+      it("should revert when given an invalid DevDAO token ID", async () => {
+        await expect(contract.mintWithDevDaoToken(8001)).to.be.revertedWith(
+          "Not a valid Dev DAO Token ID."
+        );
+      });
+
+      it("should revert when given a DevDAO token ID of 0", async () => {
+        await expect(contract.mintWithDevDaoToken(0)).to.be.revertedWith(
+          "Not a valid Dev DAO Token ID."
+        );
+      });
+
+      it("should not revert when given a valid DevDAO token ID", async () => {
+        await expect(contract.mintWithDevDaoToken(6300)).not.to.be.revertedWith(
+          "Not a valid Dev DAO Token ID."
+        );
+      });
+    });
+
+    describe("modifier: devDaoTokenOwnerOf", () => {
+      // 
+    });
+
+    // it("should revert when given an invalid DevDAO token ID", async () => {
+    //   await expect(contract.mintWithDevDaoToken(8001)).to.be.revertedWith(
+    //     "Not a valid Dev DAO Token ID."
+    //   );
+    // });
   });
 });
