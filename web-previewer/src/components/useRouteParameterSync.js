2516de3aa10e16657bafbc68323d46d9761aa67d
Carlo Miguel Dy <45052332+carlomigueldy@users.noreply.github.com>
2021-10-26 06:24:26 +0800

    Add sync query params support (#28)
    * Add vue-router@4
    
    * Define vue router and routes
    
    * Add `onMounted` and initialize from there, also grabbing query param and set initial dev ID value
    
    * Add `useRouteParameterSync`
    
    * Define vue router and routes
    
    * Add `onMounted` and initialize from there, also grabbing query param and set initial dev ID value
    
    * Add `useRouteParameterSync`
    
    * Add vue-router@4
    
    * Squash?
    
    * Reverted `usePreviewState`, and squash and merge
    
    * Revert and rename query param name

---
+++ web-previewer/src/components/useRouteParameterSync.js
@@ -0,0 +1,17 @@
+import { watch, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+export default function useRouteParameterSync(previewState) {
+  const route = useRoute();
+  const router = useRouter();
+
+  onMounted(() => {
+    if (route.query.developerId) {
+      previewState.developer.value = route.query.developerId;
+    }
+  });
+
+  watch(previewState?.developer, (developer, previousdeveloper) => {
+    router.push({ path: "/", query: { developerId: developer } });
+  });
+}
