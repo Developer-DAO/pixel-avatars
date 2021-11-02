685c2e6cfaf6348ca4d15c8a88c29842769b4622
carlomigueldy <carlomigueldy@gmail.com>
2021-11-02 09:25:20 +0800

    Comment out test

--- pixel-avatar-hardhat/test/pixel-avatars.spec.ts
+++ pixel-avatar-hardhat/test/pixel-avatars.spec.ts
@@ -1,32 +1,23 @@
 import { MockProvider } from "@ethereum-waffle/provider";
 import { expect } from "chai";
-import { ethers, waffle } from "hardhat";
+import { ethers } from "hardhat";
 import {
   LootInterface,
   PixelAvatars,
   PixelAvatars__factory,
 } from "../typechain";
-import PixelAvatarsJSON from "../artifacts/contracts/PixelAvatars.sol/PixelAvatars.json";
-import { MockContract } from "@ethereum-waffle/mock-contract";
-import { smock } from "@defi-wonderland/smock";
+import { MockContractFactory, smock } from "@defi-wonderland/smock";
 
 describe("PixelAvatars", function () {
   const [sender, receiver] = new MockProvider().getWallets();
   let contract: PixelAvatars;
-  let mockContract: MockContract;
+  let mockContract: MockContractFactory<PixelAvatars__factory>;
 
   beforeEach(async () => {
-    const contractFactory = new ethers.ContractFactory(
-      PixelAvatarsJSON.abi,
-      PixelAvatarsJSON.bytecode,
-      sender
-    );
+    const contractFactory = await ethers.getContractFactory("PixelAvatars");
+    contract = await contractFactory.deploy();
 
-    mockContract = await waffle.deployMockContract(
-      sender,
-      PixelAvatarsJSON.abi
-    );
-    contract = (await contractFactory.deploy()) as PixelAvatars;
+    mockContract = await smock.mock<PixelAvatars__factory>("PixelAvatars");
   });
 
   describe("mintWithDevDaoToken", () => {
@@ -51,16 +42,25 @@ describe("PixelAvatars", function () {
     });
 
     describe("modifier: devDaoTokenOwnerOf", () => {
-      it("should revert when given a DevDAO token ID that isn't the owner", async () => {
-        const fakeLootInterface = await smock.fake<LootInterface>(
-          "LootInterface"
-        );
-        fakeLootInterface.ownerOf.whenCalledWith(6300).returns(true);
+      /** Use forked Mainnet for testing this */
+      it("...", async () => {
+        // const devDaoTokenOwner = await ethers.getSigner(
+        //   "0x3E8c686F499C877D8f4aFB1215b6f0935796b986"
+        // );
+        // const fakeLootInterface = await smock.fake<LootInterface>(
+        //   "LootInterface"
+        // );
+        // fakeLootInterface.ownerOf.whenCalledWith(6300).returns(true);
+        // const fakePixelAvatars = await smock.fake("PixelAvatars", {
+        //   address: devDaoTokenOwner.address,
+        // });
+        // contract = contract.connect(fakePixelAvatars.wallet);
 
-        await expect(contract.mintWithDevDaoToken(6300)).emit(
-          contract,
-          "LogTokenMinted"
-        );
+        await expect(
+          contract.mintWithDevDaoToken(6300, {
+            value: ethers.utils.parseEther("0.5"),
+          })
+        ).emit(contract, "LogTokenMinted");
       });
     });
   });
