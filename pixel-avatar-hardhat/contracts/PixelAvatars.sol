e4df362fc4ec83eadd593a80d08a0ed44c39cedd
carlomigueldy <carlomigueldy@gmail.com>
2021-11-02 09:21:21 +0800

    Bump Solidity version

--- pixel-avatar-hardhat/contracts/PixelAvatars.sol
+++ pixel-avatar-hardhat/contracts/PixelAvatars.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
