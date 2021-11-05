8434bd458f41021c0b52b9b1bc56f7dcaee6ad22
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2021-11-05 20:54:21 +0100

    Tweak prettier config for 4 spaces

--- web-previewer/cli/assign-backgrounds.js
+++ web-previewer/cli/assign-backgrounds.js
@@ -1,8 +1,8 @@
-import { keyBy, sample } from "lodash-es";
-import { buildValue, default as traits, slugify } from "../data/traits.js";
-import developers from "../data/developers.js";
-import fs from "fs";
-import path from "path";
+import { keyBy, sample } from 'lodash-es'
+import { buildValue, default as traits, slugify } from '../data/traits.js'
+import developers from '../data/developers.js'
+import fs from 'fs'
+import path from 'path'
 
 /*
  * Script can be invoked by running `yarn assign-backgrounds`.
@@ -11,78 +11,78 @@ import path from "path";
  */
 
 const backgrounds = [
-  "Blue",
-  "Gray",
-  "Green",
-  "Orange",
-  "Pink",
-  "Purple",
-  "Yellow",
-].map(buildValue);
+    'Blue',
+    'Gray',
+    'Green',
+    'Orange',
+    'Pink',
+    'Purple',
+    'Yellow',
+].map(buildValue)
 
 const newDevelopers = developers.map((developer) => {
-  return {
-    ...developer,
-    background: sample(getCompatibleBackgrounds(developer)).name,
-  };
-});
+    return {
+        ...developer,
+        background: sample(getCompatibleBackgrounds(developer)).name,
+    }
+})
 
 fs.writeFile(
-  path.resolve("data/developers.js"),
-  "export default " + JSON.stringify(newDevelopers, null, 4),
-  () => console.log("Successfully updated developers.json!")
-);
+    path.resolve('data/developers.js'),
+    'export default ' + JSON.stringify(newDevelopers, null, 4),
+    () => console.log('Successfully updated developers.json!')
+)
 
 function getCompatibleBackgrounds(developer) {
-  let compatibleBackgrounds = keyBy(backgrounds, "slug");
-  const vibe = slugify(developer.vibe);
-  const clothing = slugify(developer.clothing);
+    let compatibleBackgrounds = keyBy(backgrounds, 'slug')
+    const vibe = slugify(developer.vibe)
+    const clothing = slugify(developer.clothing)
 
-  if (vibe === "jongold") {
-    delete compatibleBackgrounds.blue;
-  }
+    if (vibe === 'jongold') {
+        delete compatibleBackgrounds.blue
+    }
 
-  if (vibe === "kind") {
-    delete compatibleBackgrounds.blue;
-  }
+    if (vibe === 'kind') {
+        delete compatibleBackgrounds.blue
+    }
 
-  if (vibe === "generous") {
-    delete compatibleBackgrounds.yellow;
-  }
+    if (vibe === 'generous') {
+        delete compatibleBackgrounds.yellow
+    }
 
-  if (vibe === "hyper") {
-    delete compatibleBackgrounds.orange;
-  }
+    if (vibe === 'hyper') {
+        delete compatibleBackgrounds.orange
+    }
 
-  if (vibe === "cosmic") {
-    delete compatibleBackgrounds.purple;
-    delete compatibleBackgrounds.green;
-  }
+    if (vibe === 'cosmic') {
+        delete compatibleBackgrounds.purple
+        delete compatibleBackgrounds.green
+    }
 
-  if (vibe === "phobia") {
-    delete compatibleBackgrounds.purple;
-    delete compatibleBackgrounds.green;
-  }
+    if (vibe === 'phobia') {
+        delete compatibleBackgrounds.purple
+        delete compatibleBackgrounds.green
+    }
 
-  if (clothing === "conferencenet") {
-    delete compatibleBackgrounds.orange;
-  }
+    if (clothing === 'conferencenet') {
+        delete compatibleBackgrounds.orange
+    }
 
-  if (clothing === "whitetanktop") {
-    delete compatibleBackgrounds.grey;
-  }
+    if (clothing === 'whitetanktop') {
+        delete compatibleBackgrounds.grey
+    }
 
-  if (clothing === "platinumtrenchcoat") {
-    delete compatibleBackgrounds.grey;
-  }
+    if (clothing === 'platinumtrenchcoat') {
+        delete compatibleBackgrounds.grey
+    }
 
-  if (clothing === "bubblegumwrapper") {
-    delete compatibleBackgrounds.pink;
-  }
+    if (clothing === 'bubblegumwrapper') {
+        delete compatibleBackgrounds.pink
+    }
 
-  if (clothing === "pinkhoodie") {
-    delete compatibleBackgrounds.pink;
-  }
+    if (clothing === 'pinkhoodie') {
+        delete compatibleBackgrounds.pink
+    }
 
-  return Object.values(compatibleBackgrounds);
+    return Object.values(compatibleBackgrounds)
 }
--- web-previewer/src/components/useWalletState.js
+++ web-previewer/src/components/useWalletState.js
@@ -1,38 +1,38 @@
-import { computed, ref } from "vue";
-import Web3 from "web3";
+import { computed, ref } from 'vue'
+import Web3 from 'web3'
 
 export default function useWalletState() {
-  const address = ref(null);
-  const tokens = ref(null);
-  const isConnected = computed(() => address.value !== null);
+    const address = ref(null)
+    const tokens = ref(null)
+    const isConnected = computed(() => address.value !== null)
 
-  return {
-    address,
-    isConnected,
-    tokens,
+    return {
+        address,
+        isConnected,
+        tokens,
 
-    async connect() {
-      if (!window.ethereum) {
-        alert(
-          "MetaMask not detected. Please try again from a MetaMask enabled browser."
-        );
+        async connect() {
+            if (!window.ethereum) {
+                alert(
+                    'MetaMask not detected. Please try again from a MetaMask enabled browser.'
+                )
 
-        return;
-      }
+                return
+            }
 
-      const web3 = new Web3(window.ethereum);
+            const web3 = new Web3(window.ethereum)
 
-      address.value = (await web3.eth.requestAccounts())[0];
+            address.value = (await web3.eth.requestAccounts())[0]
 
-      // TODO - detect tokens from genesis contract
-      tokens.value = [123, 234, 345, 456];
-    },
+            // TODO - detect tokens from genesis contract
+            tokens.value = [123, 234, 345, 456]
+        },
 
-    disconnect() {
-      address.value = null;
-      tokens.value = [];
-    },
+        disconnect() {
+            address.value = null
+            tokens.value = []
+        },
 
-    claim(token) {},
-  };
+        claim(token) {},
+    }
 }
