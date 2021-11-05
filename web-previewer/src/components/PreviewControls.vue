c0b8c28a777a603421faf5a4d5645a22f8338554
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2021-11-05 20:40:40 +0100

    Implement basic claim flow frontend

--- web-previewer/src/components/PreviewControls.vue
+++ web-previewer/src/components/PreviewControls.vue
@@ -1,82 +1,80 @@
 <script setup>
-const props = defineProps({
+import { defineProps } from "vue";
+
+defineProps({
   state: Object,
 });
 </script>
 
 <template>
-  <div class="space-y-10">
-    <div class="flex flex-col space-y-1">
-      <h3 class="text-sm font-bold text-gray-600 uppercase tracking-2">
-        Developer
-      </h3>
+  <div class="w-full flex flex-col space-y-1">
+    <h3 class="text-sm font-bold text-gray-600 uppercase tracking-2">
+      Developer
+    </h3>
 
-      <input
-        id="developerNo"
-        type="number"
-        class="input-text text-center"
-        placeholder="Search no"
-        v-model="state.developer.value"
-        @change="state.updateTraits"
-      />
+    <input
+      id="developerNo"
+      v-model="state.developer.value"
+      type="number"
+      class="input-text text-center"
+      placeholder="Search no"
+      @change="state.updateTraits"
+    />
+  </div>
+
+  <div class="w-full flex flex-col space-y-2">
+    <h3 class="text-sm font-bold text-gray-600 uppercase tracking-2">Traits</h3>
+
+    <div v-for="trait of state.dataTraits" :key="trait.slug" class="relative">
+      <select
+        v-model="state.traits[trait.slug]"
+        dir="rtl"
+        class="input-select !pr-12"
+        @change="state.updateDeveloper"
+      >
+        <option
+          v-for="value in trait.values"
+          :key="value.slug"
+          :value="value.slug"
+          v-text="value.name"
+        />
+      </select>
+      <div
+        class="
+          absolute
+          left-0
+          top-0
+          bottom-1
+          flex
+          items-center
+          text-sm text-gray-600
+        "
+      >
+        <span v-text="trait.name" />
+      </div>
     </div>
 
-    <div class="flex flex-col space-y-2">
-      <h3 class="text-sm font-bold text-gray-600 uppercase tracking-2">
-        Traits
-      </h3>
-
-      <div class="relative" v-for="trait of state.dataTraits" :key="trait.slug">
-        <select
-          dir="rtl"
-          class="input-select !pr-12"
-          v-model="state.traits[trait.slug]"
-          @change="state.updateDeveloper"
-        >
-          <option
-            v-for="value in trait.values"
-            v-text="value.name"
-            :value="value.slug"
-            :key="value.slug"
-          />
-        </select>
-        <div
-          class="
-            absolute
-            left-0
-            top-0
-            bottom-1
-            flex
-            items-center
-            text-sm text-gray-600
-          "
-        >
-          <span v-text="trait.name" />
-        </div>
-      </div>
-
-      <div class="relative">
-        <select
-          dir="rtl"
-          class="input-select !pr-12"
-          v-model="state.computer.value"
-        >
-          <option :value="1">Yes</option>
-          <option :value="0">No</option>
-        </select>
-        <div
-          class="
-            absolute
-            left-0
-            top-0
-            bottom-1
-            flex
-            items-center
-            text-sm text-gray-600
-          "
-        >
-          <span>Computer</span>
-        </div>
+    <div class="relative">
+      <select
+        v-model="state.computer.value"
+        dir="rtl"
+        class="input-select !pr-12"
+      >
+        <option :value="1">Yes</option>
+        <option :value="0">No</option>
+      </select>
+      <div
+        class="
+          absolute
+          left-0
+          top-0
+          bottom-1
+          flex
+          items-center
+          text-sm text-gray-600
+        "
+      >
+        <span>Computer</span>
       </div>
     </div>
   </div>
--- web-previewer/src/pages/Home.vue
+++ web-previewer/src/pages/Home.vue
@@ -1,35 +1,57 @@
 <script setup>
+import ClaimControls from "../components/ClaimControls.vue";
 import DaoLogo from "../components/DaoLogo.vue";
 import Preview from "../components/Preview.vue";
 import PreviewControls from "../components/PreviewControls.vue";
+import ConnectWallet from "../components/ConnectWallet.vue";
 import usePreviewState from "../components/usePreviewState";
-import useRouteParameterSync from "../components/useRouteParameterSync";
+import useWalletState from "../components/useWalletState";
 
 const previewState = usePreviewState();
-useRouteParameterSync(previewState);
+const walletState = useWalletState();
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col sm:flex-row">
-    <div
-      class="sm:w-2/5 bg-white flex flex-col items-center space-y-10 py-10 px-4"
-    >
-      <DaoLogo class="w-32 h-32 block mx-auto rounded-full shadow-md" />
+    <div class="sm:w-2/5 bg-white py-10 px-4 flex">
+      <div
+        class="
+          flex flex-col
+          items-center
+          justify-center
+          space-y-10
+          w-full
+          max-w-md
+          mx-auto
+        "
+      >
+        <div class="flex justify-between items-center w-full">
+          <DaoLogo class="w-16 h-16 rounded-full shadow-md" />
 
-      <PreviewControls :state="previewState" class="w-full max-w-xs" />
+          <ConnectWallet :state="walletState" />
+        </div>
+
+        <ClaimControls
+          :preview-state="previewState"
+          :wallet-state="walletState"
+          class="w-full max-w-md"
+        />
+
+        <PreviewControls :state="previewState" class="w-full max-w-md" />
+      </div>
     </div>
 
-    <div
-      class="
-        sm:w-3/5
-        bg-gray-100
-        border-l border-gray-200
-        flex
-        items-center
-        justify-center
-      "
-    >
-      <Preview :state="previewState" class="w-full max-w-md" />
+    <div class="sm:w-3/5 bg-gray-100 border-l border-gray-200">
+      <div
+        class="
+          flex
+          items-center
+          justify-center
+          sm:min-h-screen sm:sticky sm:top-0
+        "
+      >
+        <Preview :state="previewState" class="w-full max-w-md" />
+      </div>
     </div>
   </div>
 </template>
