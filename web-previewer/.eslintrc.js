d6c3fcab5685681494acdf188b67e50d737b42c3
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2021-11-05 20:39:46 +0100

    Change to using webpack

---
+++ web-previewer/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        'vue/no-mutating-props': 0,
+        'vue/require-default-prop': 0,
+    }
+}
