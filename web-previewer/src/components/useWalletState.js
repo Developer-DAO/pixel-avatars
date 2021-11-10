dc8b5a20f4d8da88d7347e69d8b5554f84c9956b
Alex Plutta <alex.plutta@googlemail.com>
2021-11-10 20:19:25 +0100

    replace web3 with ethers

--- web-previewer/src/components/useWalletState.js
+++ web-previewer/src/components/useWalletState.js
@@ -1,9 +1,11 @@
-import { computed, ref } from 'vue'
-import Web3 from 'web3'
+import {computed, ref} from 'vue'
+import {ethers} from "ethers";
+
 
 export default function useWalletState() {
     const address = ref(null)
-    const tokens = ref(null)
+    const tokens = ref([])
+    const provider = ref(null);
     const isConnected = computed(() => address.value !== null)
 
     return {
@@ -12,20 +14,10 @@ export default function useWalletState() {
         tokens,
 
         async connect() {
-            if (!window.ethereum) {
-                alert(
-                    'MetaMask not detected. Please try again from a MetaMask enabled browser.'
-                )
-
-                return
-            }
-
-            const web3 = new Web3(window.ethereum)
-
-            address.value = (await web3.eth.requestAccounts())[0]
-
-            // TODO - detect tokens from genesis contract
-            tokens.value = [123, 234, 345, 456]
+            const [_address] = await window.ethereum.request({method: "eth_requestAccounts"})
+            provider.value = new ethers.providers.Web3Provider(window.ethereum);
+            address.value = _address
+            console.log(address)
         },
 
         disconnect() {
@@ -33,6 +25,7 @@ export default function useWalletState() {
             tokens.value = []
         },
 
-        claim(token) {},
+        claim(token) {
+        },
     }
 }
