ec603de027b756d18c111a7569cf27875f6715e0
Alex Plutta <alex.plutta@googlemail.com>
2021-11-10 21:17:17 +0100

    fetch tokens owned by the user from the mainnet developer dao contract

--- web-previewer/src/components/useWalletState.js
+++ web-previewer/src/components/useWalletState.js
@@ -1,11 +1,11 @@
 import {computed, ref} from 'vue'
 import {ethers} from "ethers";
-
+import {GENESIS_CONTRACT} from "../constants/adresses";
+import GenesisContract from "./../GenesisContract.json";
 
 export default function useWalletState() {
     const address = ref(null)
     const tokens = ref([])
-    const provider = ref(null);
     const isConnected = computed(() => address.value !== null)
 
     return {
@@ -15,9 +15,32 @@ export default function useWalletState() {
 
         async connect() {
             const [_address] = await window.ethereum.request({method: "eth_requestAccounts"})
-            provider.value = new ethers.providers.Web3Provider(window.ethereum);
             address.value = _address
-            console.log(address)
+            await this.fetchOwnersTokens();
+        },
+
+        async fetchOwnersTokens() {
+            if (address === null) {
+                console.log("please connect to wallet and select an account");
+                return;
+            }
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+            //developer dao contract
+            const contract = new ethers.Contract(GENESIS_CONTRACT, GenesisContract.abi, provider);
+
+            //number of tokens owned by the address
+            const balance = await contract.balanceOf(address.value)
+
+            //For each token the address owns we need to fetch the actual nft
+            const tokenPromises = [...Array(balance).keys()].map(idx => contract.tokenOfOwnerByIndex(address.value, idx));
+
+            //await all async calls
+            const _tokens = await Promise.all(tokenPromises);
+
+            //map them to string
+            tokens.value = _tokens.map(t => t.toString());
+
         },
 
         disconnect() {
---
+++ web-previewer/src/constants/adresses.js
@@ -0,0 +1,2 @@
+//Developer Daos Genesis contract on Mainnet
+export const GENESIS_CONTRACT = "0x25ed58c027921E14D86380eA2646E3a1B5C55A8b"
