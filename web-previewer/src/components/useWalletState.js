7ef22850fdbe2131230b9a55413d6f9425f6e11b
Alex Plutta <alex.plutta@googlemail.com>
2021-11-11 23:33:00 +0100

    add method call to mintWithDevDaoToken

--- web-previewer/src/components/useWalletState.js
+++ web-previewer/src/components/useWalletState.js
@@ -1,7 +1,8 @@
 import {computed, ref} from 'vue'
 import {ethers} from "ethers";
-import {GENESIS_CONTRACT} from "../constants/adresses";
+import {GENESIS_CONTRACT, NETWORK, PIXEL_AVATAR_CONTRACT} from "../constants/adresses";
 import GenesisContract from "./../GenesisContract.json";
+import PixelAvatarContract from "./../PixelAvatars.json";
 
 export default function useWalletState() {
     const address = ref(null)
@@ -24,13 +25,13 @@ export default function useWalletState() {
                 console.log("please connect to wallet and select an account");
                 return;
             }
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const provider = new ethers.providers.Web3Provider(window.ethereum, NETWORK);
 
             //developer dao contract
             const contract = new ethers.Contract(GENESIS_CONTRACT, GenesisContract.abi, provider);
 
             //number of tokens owned by the address
-            const balance = await contract.balanceOf(address.value)
+            const balance = (await contract.balanceOf(address.value)).toNumber();
 
             //For each token the address owns we need to fetch the actual nft
             const tokenPromises = [...Array(balance).keys()].map(idx => contract.tokenOfOwnerByIndex(address.value, idx));
@@ -48,7 +49,17 @@ export default function useWalletState() {
             tokens.value = []
         },
 
-        claim(token) {
+        async claim(token) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, NETWORK);
+
+            const signer = provider.getSigner();
+
+
+            //pixel avatar contract
+            const contract = new ethers.Contract(PIXEL_AVATAR_CONTRACT, PixelAvatarContract.abi, signer);
+
+            const transaction = await contract.mintWithDevDaoToken(token, {value: ethers.utils.parseEther("0.1")});
+            await transaction.wait()
 
         },
     }
--- web-previewer/src/constants/adresses.js
+++ web-previewer/src/constants/adresses.js
@@ -1,2 +1,6 @@
-//Developer Daos Genesis contract on Mainnet
+//Developer Daos Genesis contract
 export const GENESIS_CONTRACT = "0x25ed58c027921E14D86380eA2646E3a1B5C55A8b"
+//PixelAvatar Contract
+export const PIXEL_AVATAR_CONTRACT = "0x8F0EEfABBF5164453D08081c063c1408d08f8c7f"
+
+export const NETWORK = "rinkeby";
