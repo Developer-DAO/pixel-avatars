e5dee660f93af4a9fab09bac3542b33cd97d860d
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2021-11-22 18:30:51 +0100

    Tweak layout to use dropdown for connect

--- web-previewer/src/components/ConnectWallet.vue
+++ web-previewer/src/components/ConnectWallet.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/solid'
 import { inject } from 'vue'
 import {SUPPORTED_PROVIDERS} from './useWeb3Provider';
 
@@ -6,20 +8,53 @@ const state = inject('walletState')
 </script>
 
 <template>
-    <button
-        v-if="!state.isConnected.value"
-        class="bg-black text-white text-sm py-2 rounded px-4 inline-block"
-        @click="state.connect(SUPPORTED_PROVIDERS.META_MASK)"
-    >
-        Connect Metamask
-    </button>
-    <button
-        v-if="!state.isConnected.value"
-        class="bg-black text-white text-sm py-2 rounded px-4 inline-block"
-        @click="state.connect(SUPPORTED_PROVIDERS.WALLET_CONNECT)"
-    >
-        Connect with Wallet Connect
-    </button>
+<!--    <button-->
+<!--        v-if="!state.isConnected.value"-->
+<!--        class="bg-black text-white text-sm py-2 rounded px-4 inline-block"-->
+<!--        @click="state.connect(SUPPORTED_PROVIDERS.META_MASK)"-->
+<!--    >-->
+<!--        Connect Metamask-->
+<!--    </button>-->
+<!--    <button-->
+<!--        v-if="!state.isConnected.value"-->
+<!--        class="bg-black text-white text-sm py-2 rounded px-4 inline-block"-->
+<!--        @click="state.connect(SUPPORTED_PROVIDERS.WALLET_CONNECT)"-->
+<!--    >-->
+<!--        Connect with Wallet Connect-->
+<!--    </button>-->
+
+
+    <Menu v-if="!state.isConnected.value" as="div" class="relative inline-block text-left">
+        <div>
+            <MenuButton class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-black text-white text-sm">
+                Connect wallet
+                <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </MenuButton>
+        </div>
+
+        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+            <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="py-1">
+                    <MenuItem v-slot="{ active }">
+                        <button
+                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full text-left px-4 py-2 text-sm']"
+                            @click="state.connect(SUPPORTED_PROVIDERS.META_MASK)"
+                        >
+                            MetaMask
+                        </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <button
+                            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full text-left px-4 py-2 text-sm']"
+                            @click="state.connect(SUPPORTED_PROVIDERS.WALLET_CONNECT)"
+                        >
+                            Wallet Connect
+                        </button>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
 
     <div v-else class="flex-1 min-w-0 flex flex-col items-end space-y-1">
         <span class="text-sm text-gray-500">Connected to</span>
