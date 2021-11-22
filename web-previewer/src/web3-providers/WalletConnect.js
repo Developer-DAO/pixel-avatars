869130b7d74e1e0c88b51bc9983539d1f15ccef2
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2021-11-22 18:08:55 +0100

    Refactor wallet integration with different providers

---
+++ web-previewer/src/web3-providers/WalletConnect.js
@@ -0,0 +1,26 @@
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { INFURA_ID } from '../constants/adresses'
+
+export default class WalletConnect {
+    constructor() {
+        this.provider = null
+    }
+
+    async connect() {
+        this.provider = new WalletConnectProvider({
+            infuraId: INFURA_ID,
+        })
+
+        await this.provider.enable()
+
+        return this.provider.accounts[0] ?? null
+    }
+
+    async disconnect() {
+        if (this.provider) {
+            await this.provider.disconnect()
+        }
+
+        this.provider = null
+    }
+}
