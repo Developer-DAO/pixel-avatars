82c1906e517de8c1655ed74711f8e39a4d86c971
Brian Gershon <briangershon@users.noreply.github.com>
2021-12-29 15:50:48 -0800

    Bring in Rasmus’ latest hardhat configuration.

--- pixel-avatar-hardhat/hardhat.config.ts
+++ pixel-avatar-hardhat/hardhat.config.ts
@@ -1,73 +1,65 @@
-import * as dotenv from "dotenv";
+import { HardhatUserConfig, task } from 'hardhat/config'
+import { HttpNetworkUserConfig } from 'hardhat/types'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import * as dotenv from 'dotenv'
+import * as process from 'process'
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
-dotenv.config();
+dotenv.config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-const MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL ?? "";
-const RINKEYBY_RPC_URL = process.env.ALCHEMY_RINKEBY_RPC_URL ?? "";
-// const MUMBAI_RPC_URL = process.env.ALCHEMY_MUMBAI_RPC_URL ?? "";
-
-const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "12345678901234567890123456789012";
-// const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY ?? "12345678901234567890123456789012";
-// const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY ?? "12345678901234567890123456789012";
-
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+const config: HardhatUserConfig = (() => {
+    function networkConfiguration(
+        url: string | undefined,
+        account: string | undefined
+    ): HttpNetworkUserConfig {
+        return {
+            url: url ?? '',
+            accounts: account ? [`0x${account}`] : 'remote',
+        }
+    }
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      // forking: {
-      //   url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      // },
-    },
+    return {
+        solidity: '0.8.9',
+        defaultNetwork: 'localhost',
+        networks: {
+            hardhat: {},
 
-    // mumbai: {
-    //   url: MUMBAI_RPC_URL,
-    //   accounts: [`0x${MUMBAI_PRIVATE_KEY}`],
-    // },
+            mumbai: networkConfiguration(
+                process.env.MUMBAI_RPC_URL,
+                process.env.MUMBAI_PRIVATE_KEY
+            ),
 
-    // rinkeby: {
-    //   url: RINKEYBY_RPC_URL,
-    //   accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
-    // },
+            mainnet: networkConfiguration(
+                process.env.MAINNET_RPC_URL,
+                process.env.MAINNET_PRIVATE_KEY
+            ),
+        },
 
-    // mainnet: {
-    //   url: MAINNET_RPC_URL,
-    //   accounts: [`0x${PRIVATE_KEY}`],
-    // },
-  },
+        etherscan: {
+            // Your API key for Etherscan. Obtain one at https://etherscan.io/
+            apiKey: process.env.ETHERSCAN_API_KEY ?? '',
+        },
 
-  etherscan: {
-    // Your API key for Etherscan. Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY,
-  },
+        gasReporter: {
+            enabled: process.env.REPORT_GAS !== undefined,
+            currency: 'USD',
+        },
+    }
+})()
 
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-};
-
-export default config;
+export default config
