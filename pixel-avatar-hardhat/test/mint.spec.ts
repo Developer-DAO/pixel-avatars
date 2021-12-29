c48bd78865281049912b4d62a1c0eabb69d20ade
Brian Gershon <briangershon@users.noreply.github.com>
2021-12-28 19:34:04 -0800

    Add note about test private key.

--- pixel-avatar-hardhat/test/mint.spec.ts
+++ pixel-avatar-hardhat/test/mint.spec.ts
@@ -19,6 +19,8 @@ describe("PixelAvatars", function () {
       expect(signer.address.toLowerCase()).to.be.equal(
         "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266".toLowerCase()
       );
+      // no worries here checking in this test private key.
+      // It's part of the hardhat network test accounts and is not secret.
       const PRIVATE_KEY =
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
       const address = signer.address;
