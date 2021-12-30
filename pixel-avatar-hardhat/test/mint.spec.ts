3308cf2b5c9059cddca07dfe2752bc06e39e405e
Brian Gershon <briangershon@users.noreply.github.com>
2021-12-29 16:40:05 -0800

    Move all the auth setup into beforeEach() and add tests to confirm error events.

--- pixel-avatar-hardhat/test/mint.spec.ts
+++ pixel-avatar-hardhat/test/mint.spec.ts
@@ -1,6 +1,15 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import {
+  // EventFilter,
+  // Signer,
+  // BigNumber,
+  BigNumberish,
+  // PopulatedTransaction,
+  Signature,
+} from "ethers";
 import { PixelAvatars } from "../typechain";
+
 const utils = require("../../web-server/src/utils");
 
 describe("PixelAvatars", function () {
@@ -11,50 +20,86 @@ describe("PixelAvatars", function () {
     contract = await contractFactory.deploy();
   });
 
-  describe("mintWithSignature()", () => {
-    it("should mint a token when given proper auth and minimum ether", async () => {
-      const [signer] = await ethers.getSigners();
-      const address = signer.address;
-
-      // these test keys generated via `node web-server/cli/generate-keys.js`
-      const PRIVATE_KEY =
-        "8ef55fc4bb8f066ada9a9f1cbf6f09ff8a1ed9b168c9eeec0ca0a8fda8b309fd";
-      const serverAddress = "0x9788f6B80e3b856bb420eDf05D352f474be733a5";
-
-      // data returned from web server
+  describe("mintWithSignature()", function () {
+    it("should fail without proper auth", async () => {
       const tokenId = 6300;
-      const AUTHORIZATION_LIFETIME_SECONDS = 5 * 60;
-
-      const deadline =
-        Math.round(Date.now() / 1000) + AUTHORIZATION_LIFETIME_SECONDS;
-
-      const message = utils.hexConcat([
-        "TokenId:",
-        tokenId.toString(),
-        "Address:",
-        address,
-        "Deadline:",
-        deadline.toString(),
-      ]);
-      const hash = utils.hash(message);
-      const signature = utils.sign(hash, PRIVATE_KEY);
-
-      const split = ethers.utils.splitSignature(signature);
-
-      await contract.setServerAddress(serverAddress);
+      const deadline = 0;
+      const v = "0x1b";
+      const r =
+        "0xfd3a030273cf43afadb504375fcf378003c506ae8aad2c5b651e832af7e87012";
+      const s =
+        "0x2f86d81471da2a7077229fd7804536c19166496813aeeea31bb603e16fbf828b";
 
       await expect(
-        contract.mintWithSignature(
-          tokenId,
-          deadline,
-          split.v,
-          split.r,
-          split.s,
-          {
-            value: ethers.utils.parseEther("6"),
-          }
-        )
-      ).emit(contract, "LogTokenMinted");
+        contract.mintWithSignature(tokenId, deadline, v, r, s, {
+          value: ethers.utils.parseEther("6"),
+        })
+      ).to.be.revertedWith("Invalid server signature");
+    });
+
+    describe("with proper auth", function () {
+      let tokenId: BigNumberish;
+      let deadline: number;
+      let split: Signature;
+
+      beforeEach(async () => {
+        const [signer] = await ethers.getSigners();
+        const address = signer.address;
+
+        // these test keys generated via `node web-server/cli/generate-keys.js`
+        const PRIVATE_KEY =
+          "8ef55fc4bb8f066ada9a9f1cbf6f09ff8a1ed9b168c9eeec0ca0a8fda8b309fd";
+        const serverAddress = "0x9788f6B80e3b856bb420eDf05D352f474be733a5";
+
+        // data returned from web server
+        tokenId = 6300;
+        const AUTHORIZATION_LIFETIME_SECONDS = 5 * 60;
+
+        deadline =
+          Math.round(Date.now() / 1000) + AUTHORIZATION_LIFETIME_SECONDS;
+
+        const message = utils.hexConcat([
+          "TokenId:",
+          tokenId.toString(),
+          "Address:",
+          address,
+          "Deadline:",
+          deadline.toString(),
+        ]);
+        const hash = utils.hash(message);
+        const signature = utils.sign(hash, PRIVATE_KEY);
+
+        split = ethers.utils.splitSignature(signature);
+
+        await contract.setServerAddress(serverAddress);
+      });
+
+      it("should fail if not enough eth sent", async () => {
+        await expect(
+          contract.mintWithSignature(
+            tokenId,
+            deadline,
+            split.v,
+            split.r,
+            split.s
+          )
+        ).to.be.revertedWith("Not enough ether sent");
+      });
+
+      it("should mint a token when given minimum ether", async () => {
+        await expect(
+          contract.mintWithSignature(
+            tokenId,
+            deadline,
+            split.v,
+            split.r,
+            split.s,
+            {
+              value: ethers.utils.parseEther("6"),
+            }
+          )
+        ).emit(contract, "LogTokenMinted");
+      });
     });
   });
 });
